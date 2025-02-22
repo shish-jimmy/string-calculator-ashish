77fc4a22d6e78d923c54ee5d683312bc439d4cc6
Ashish Brahmbhatt <brahmbhatt.ashish8769@gmail.com>
2025-02-22 16:31:17 +0530

    adding unkonwn number fails

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -11,4 +11,8 @@ describe("StringCalculator Kata", () => {
   it("should return the sum of two numbers", () => {
     expect(add("1,2")).toBe(3);
   });
+  it("should return the sum of multiple numbers", () => {
+    expect(add("1,2,3,4,5,8,0")).toBe(15);
+  });
+
 });
