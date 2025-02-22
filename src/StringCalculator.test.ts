4dab8a9c8cd54945c91903a7694e3d1b0254bc31
Ashish Brahmbhatt <brahmbhatt.ashish8769@gmail.com>
2025-02-22 16:53:19 +0530

    adding multiple unknown numbers , pass

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -12,7 +12,7 @@ describe("StringCalculator Kata", () => {
     expect(add("1,2")).toBe(3);
   });
   it("should return the sum of multiple numbers", () => {
-    expect(add("1,2,3,4,5,8,0")).toBe(15);
+    expect(add("1,2,3,4,5,8,0")).toBe(23);
   });
 
 });
