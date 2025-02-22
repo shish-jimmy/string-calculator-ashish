be9ac8a1235f2ef67937ec02c84ae868021602b8
Ashish Brahmbhatt <brahmbhatt.ashish8769@gmail.com>
2025-02-22 16:26:00 +0530

    handles one and two numbers

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -4,4 +4,11 @@ describe("StringCalculator Kata", () => {
   it("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+  it("should return the number itself for a single number", () => {
+    expect(add("1")).toBe(1);
+  });
+
+  it("should return the sum of two numbers", () => {
+    expect(add("1,2")).toBe(3);
+  });
 });
