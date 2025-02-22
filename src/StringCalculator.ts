3ac3af462bfd20566870ceec86e6e6c3c3312feb
Ashish Brahmbhatt <brahmbhatt.ashish8769@gmail.com>
2025-02-22 16:29:24 +0530

    handles one and two numbers , fix

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,3 +1,4 @@
 export function add(numbers: string): number {
-  return numbers ? parseInt(numbers) : 0;
+    if(!numbers) return 0;
+  return numbers.split(",").reduce((sum, num) => sum + parseInt(num), 0);
 }
